9bb224a5949d271e65ca87b039e91c226b7b8bd1
Jossué Dután <jossuedutan@gmail.com>
2018-11-09 17:21:17 -0500

    Observables

--- 02-typescript/02-observables.js
+++ 02-typescript/02-observables.js
@@ -1,9 +1,59 @@
+// 02-observables.ts
 const rxjs = require('rxjs');
-const numeros$ = rxjs.of(1, 2, 3, 4, 5, 6); //observable de números
+const map = require('rxjs/operators').map;
+const distinct = require('rxjs/operators').distinct;
+const concat = require('rxjs/operators').concat;
+const numeros$ = rxjs.of(1, "Adrian", 1, true, { nombre: "Adrian" }, [1, 2, 3], new Date()); //observable de cualquier cosa
 console.log(numeros$);
 numeros$
+    .pipe(distinct(), //el orden importa
+map((valorActual) => {
+    return {
+        data: valorActual
+    };
+}))
     .subscribe((ok) => {
     console.log('En ok', ok);
+}, (error) => {
+    console.log('En error', error);
+}, () => {
+    console.log('Completado');
+});
+const promesita = (correcto) => {
+    return new Promise((resolve, reject) => {
+        if (correcto) {
+            resolve(':)');
+        }
+        else {
+            reject(':(');
+        }
+    });
+};
+const promesita$ = rxjs.from(promesita(false));
+/*
+promesita$
+    .subscribe(
+        (ok) => {
+            console.log('En promesita', ok)
+        },
+        (error) => {
+            console.log('Error en promesita', error)
+        },
+        () => {
+            console.log('Completado')
+        },
+    );
+*/
+numeros$
+    .pipe(concat(promesita$))
+    .pipe(distinct(), //el orden importa
+map((valorActual) => {
+    return {
+        data: valorActual
+    };
+}))
+    .subscribe((ok) => {
+    console.log('En concat', ok);
 }, (error) => {
     console.log('En error', error);
 }, () => {
